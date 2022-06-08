4bfd46c067a938de359f75566d5212f1575dd1c2
iamyelex <yelex4love@gmail.com>
2022-06-08 17:20:29 +0800

    modulus

---
+++ practice.py
@@ -0,0 +1,13 @@
+print("hello world")
+
+
+#use 3 quotes to print on another line
+print("""hello world
+where are you
+are you coming hom tonight?                                 says who
+we miss you""")
+
+hi = "how are"
+h2 = "you doing"
+
+print(hi, h2)
